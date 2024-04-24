6d273c107005f8737485ebbfb4dfac2b6210dd9c
Yasha Bubnov <yasha.bubnov@wolt.com>
2024-04-24 22:08:57 +0200

    Optionally include internal nodes in the output.

--- torch_geopooling/transforms.py
+++ torch_geopooling/transforms.py
@@ -1,13 +1,41 @@
+from __future__ import annotations
+
 from enum import auto
 from enum import Enum
 from typing import Tuple
 from typing import List
 from typing import Iterator
+from typing import Set
+from typing import TypeAlias
+from typing import NamedTuple
 
 from torch import Tensor
 
 
 __all__ = ["TileWKT"]
+
+
+class _Tile(NamedTuple):
+    z: int
+    x: int
+    y: int
+
+    def child(self, x: int, y: int) -> _Tile:
+        return _Tile(self.z + 1, self.x * 2 + x, self.y * 2 + y)
+
+
+class _TileSet(set):
+
+    def __init__(self, tiles: Tensor) -> None:
+        super().__init__(_Tile(*tile.detach().tolist()) for tile in tiles)
+
+    def is_terminal(self, tile: _Tile) -> bool:
+        return (
+            (tile.child(0, 0) not in self) and
+            (tile.child(0, 1) not in self) and
+            (tile.child(1, 0) not in self) and
+            (tile.child(1, 1) not in self)
+        )
 
 
 class TileWKT:
@@ -18,11 +46,21 @@ class TileWKT:
     Args:
         exterior (tuple): Exterior coordinates in xywh format. The exterior is used to calculate
             boundaries of a tile to produce a final WKT.
+        precision (float): A precision of the resulting geometry, digits after the deciman point.
+        internal (bool): When True, output includes internal nodes of the Quadtree tiles.
+            Otherwise (default) returns only geometry of terminal nodes.
     """
 
-    def __init__(self, exterior: Tuple[float, float, float, float], precision: float = 7) -> None:
-        self.exterior = exterior
+    def __init__(
+        self,
+        exterior: Tuple[float, float, float, float],
+        precision: float = 7,
+        internal: bool = False,
+    ) -> None:
+        self.exterior = tuple(map(float, exterior))
         self.precision = precision
+        self.internal = internal
+
         self._xmin, self._ymin, self._width, self._height = exterior
 
     def __call__(self, tiles: Tensor) -> Iterator[str]:
@@ -34,17 +72,23 @@ class TileWKT:
                 f"tiles should be triplets of (z, x, y), got tensor of shape {tiles.size()}"
             )
 
+        tileset = _TileSet(tiles)
 
         for tile in tiles:
             z, x, y = tile.detach().tolist()
             width = self._width / (1 << z)
             height = self._height / (1 << z)
 
+            if (not self.internal) and (not tileset.is_terminal(_Tile(z, x, y))):
+                continue
+
             xmin = self._xmin + width * x
-            xmax = xmin + width
+            xmax = round(xmin + width, self.precision)
+            xmin = round(xmin, self.precision)
 
             ymin = self._ymin + height * y
-            ymax = ymin + height
+            ymax = round(ymin + height, self.precision)
+            ymin = round(ymin, self.precision)
 
             yield (
                 "POLYGON (("
