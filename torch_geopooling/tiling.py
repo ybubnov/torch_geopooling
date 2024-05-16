0ae1748016269f30abfe88eca7fa90840fde0fdb
Yasha Bubnov <yasha.bubnov@wolt.com>
2024-05-16 23:04:30 +0200

    Add `tiling.py` module to the PR.

---
+++ torch_geopooling/tiling.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from collections import deque
+from itertools import product
+from typing import Iterator, NamedTuple
+
+from shapely.geometry import Polygon
+
+__all__ = ["Exterior", "Tile", "regular_tiling"]
+
+
+class Tile(NamedTuple):
+    z: int
+    x: int
+    y: int
+
+    @classmethod
+    def root(cls) -> Tile:
+        return cls(0, 0, 0)
+
+    def child(self, x: int, y: int) -> Tile:
+        return Tile(self.z + 1, self.x * 2 + x, self.y * 2 + y)
+
+    def children(self) -> Iterator[Tile]:
+        for x, y in product(range(2), range(2)):
+            yield self.child(x, y)
+
+
+class Exterior(NamedTuple):
+    xmin: float
+    ymin: float
+    width: float
+    height: float
+
+    @property
+    def xmax(self) -> float:
+        return self.xmin + self.width
+
+    @property
+    def ymax(self) -> float:
+        return self.ymin + self.height
+
+    def slice(self, tile: Tile) -> Exterior:
+        w = self.width / (1 << tile.z)
+        h = self.height / (1 << tile.z)
+        return Exterior(self.xmin + tile.x * w, self.ymin + tile.y * h, w, h)
+
+    def as_polygon(self) -> Polygon:
+        return Polygon(
+            [
+                (self.xmin, self.ymin),
+                (self.xmax, self.ymin),
+                (self.xmax, self.ymax),
+                (self.xmin, self.ymax),
+            ]
+        )
+
+
+def regular_tiling(polygon: Polygon, exterior: Exterior, z: int) -> Iterator[Tile]:
+    """Returns a regular quad-tiling (tiles of the same size).
+
+    Method returns all tiles of level (z) that have a common intersection with a specified
+    polygon.
+
+    Args:
+        polygon: A polygon to cover with tiles.
+        exterior: Exterior (bounding box) of the quadtree. For example, for geospatial
+            coordinates, this will be `(-180.0, -90.0, 360.0, 180.0)`.
+        z: Zoom level of the tiles.
+
+    Returns:
+        Iterator of tiles.
+    """
+    queue = deque([Tile.root()])
+
+    while len(queue) > 0:
+        tile = queue.pop()
+
+        tile_poly = exterior.slice(tile).as_polygon()
+        if not tile_poly.intersects(polygon):
+            continue
+
+        if tile.z >= z:
+            yield tile
+        else:
+            queue.extend(tile.children())
