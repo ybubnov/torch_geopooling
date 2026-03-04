971a0708aa0e632e1291b86fdf901d283ddce528
Yakau Bubnou <girokompass@gmail.com>
2026-03-04 10:38:30 +0100

    This patch removes quadtree implementation (#92)
    It also adds "cppcheck" into the pre-commit list of checks, and updates
    the license header with SPDX tags in some c++ files.

--- include/torch_geopooling/torch_geopooling.h
+++ include/torch_geopooling/torch_geopooling.h
@@ -8,7 +8,6 @@
 #include <torch_geopooling/exception.h>
 #include <torch_geopooling/quadpool.h>
 #include <torch_geopooling/quadrect.h>
-#include <torch_geopooling/quadtree.h>
 #include <torch_geopooling/quadtree_options.h>
 #include <torch_geopooling/quadtree_set.h>
 #include <torch_geopooling/tile.h>
--- test/testing.h
+++ test/testing.h
@@ -7,7 +7,7 @@ template <typename Exception>
 std::function<bool(const Exception&)>
 exception_contains_text(const std::string error_message)
 {
-    return [&](const Exception& error) -> bool {
+    return [=](const Exception& error) -> bool {
         return std::string(error.what()).find(error_message) != std::string::npos;
     };
 }
